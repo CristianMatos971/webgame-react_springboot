a17bd7ee1ae20590d556c53555b3ad0fd9100529
CristianMatos <cristianernandes971@gmail.com>
2025-12-01 21:42:17 -0400

    initial commit, structure the architeture for springboot+react monorepo

---
+++ backend/src/test/java/com/conquerquest/backend/BackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.conquerquest.backend;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class BackendApplicationTests {
+
+	/*
+	 * @Test
+	 * void contextLoads() {
+	 * }
+	 * 
+	 */
+
+}
